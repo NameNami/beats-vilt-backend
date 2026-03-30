5a4ead35ef5535172b60cf3aed28f545898c2d5a
Nami <najmisanusi@gmail.com>
2026-03-30 17:07:21 +0800

    chore: wire up VILT

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-800">BEATS</h1>
+            <p class="text-gray-500 mt-2">BLE-Based Smart Attendance System</p>
+        </div>
+    </div>
+</template>
