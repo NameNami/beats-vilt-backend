e01c622260df14a2a3424775fae18bb54aad5d6f
Nami <najmisanusi@gmail.com>
2026-04-13 17:54:07 +0800

    feat: add beacon heartbeat API endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\Api\StudentAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\Api\BeaconHeartbeatController;
+use function Pest\Laravel\post;
 
+// Student Auth (Login & Forgot Password)
 Route::post('/student/login', [StudentAuthController::class, 'login'])->name('api.student.login');
 Route::post('/student/forgot-password', [StudentAuthController::class, 'sendResetLinkEmail']);
+
+// Beacon
+Route::post('/beacon/heartbeat', [BeaconHeartbeatController::class, 'heartbeat']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
