c1e0038b7013a0c7978e9e2f7ef473816f3a7363
Nami <najmisanusi@gmail.com>
2026-04-28 11:45:02 +0800

    feat: add AttendanceController with BLE check-in endpoint and update API routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\StudentAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\Api\BeaconHeartbeatController;
+use app\Http\Controllers\Api\AttendanceController;
 use function Pest\Laravel\post;
 
 // Student Auth (Login & Forgot Password)
@@ -16,3 +17,8 @@ Route::post('/beacon/heartbeat', [BeaconHeartbeatController::class, 'heartbeat']
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// TODO: add logout aarrrrghhhh how can i forgot about log out
+
+// Check-in
+Route::post('/student/check-in-ble', [AttendanceController::class, 'checkIn'])->name('api.student.check-in');
