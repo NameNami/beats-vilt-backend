b279058b1f92c4cbc4aaf5e98dc1c186e631c355
Nami <najmisanusi@gmail.com>
2026-05-05 00:47:25 +0800

    feat: implement Lecturer Dashboard and update attendance logic
    - Added `WebLecturerDashboardController` with `lecturerDashboard` method to render the new dashboard.
    - Introduced `WebAttendanceController` with placeholder methods for future enhancements.
    - Updated enum values for attendance status by adding `present` and adjusted related logic in `AttendanceServices`.
    - Created `AppLayout` and `LecturerDashboard` Vue components for UI improvements.
    - Updated web routes to include lecturer dashboard route.
    - Modified attendance migration to include `present` status.

---
+++ app/Http/Controllers/WebAttendanceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class WebAttendanceController extends Controller
+{
+    // TODO: make live attendance
+    // TODO: qr generator
+    // TODO: make sure to modify the is display for class session
+    // TODO: button for manual attendance
+    // TODO: export to excel
+    // TODO: mark all present button
+    // TODO: clear all button
+    // TODO: search bar for student
+}
