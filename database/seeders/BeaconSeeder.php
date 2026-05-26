aa433aaa06d26ea360834ad98db320800c2d0818
Nami <najmisanusi@gmail.com>
2026-05-27 04:02:36 +0800

    chore: update deploy workflow with image name and remote file transfer steps
    - Set `IMAGE_NAME` to a static value for consistency.
    - Added steps to copy `docker-compose.yml` to VPS and execute remote commands.

--- database/seeders/BeaconSeeder.php
+++ database/seeders/BeaconSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Beacon;
 use App\Models\Room;
-use Illuminate\Support\Str;
 
 class BeaconSeeder extends Seeder
 {
@@ -16,15 +15,9 @@ class BeaconSeeder extends Seeder
     {
         $rooms = Room::all();
         foreach ($rooms as $room) {
-            Beacon::updateOrCreate(
-                ['room_id' => $room->id],
-                [
-                    'uuid' => (string) Str::uuid(),
-                    'mac_address' => Str::random(17),
-                    'rssi_threshold' => -70,
-                    'status' => 'active',
-                ]
-            );
+            Beacon::factory()->create([
+                'room_id' => $room->id,
+            ]);
         }
     }
 }
